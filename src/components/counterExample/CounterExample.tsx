74e59fa70891b5f1cc1bd9d5fd7da7567945d82f
hexedcorp <hexconservationist@gmail.com>
2019-09-10 13:39:24 +0300

    reworked the react effect, added some state, conditionally rendering buttons for addition or subtraction

--- src/components/counterExample/CounterExample.tsx
+++ src/components/counterExample/CounterExample.tsx
@@ -3,37 +3,71 @@ import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 
 import { IStore } from '../../store/store';
-import { counterExampleChangeName, counterExampleCount } from '../../actions';
+import {
+  counterExampleChangeName,
+  counterExampleCount,
+  CounterExampleActionPayload,
+} from '../../actions';
 
 import './counterExample.scss';
 
 export interface CounterProps {
   counterExample: { name: string; count: number };
   counterExampleChangeName: (payload: string) => void;
-  counterExampleCount: () => void;
-} 
+  counterExampleCount: (payload: CounterExampleActionPayload) => void;
+}
 
 const CounterExampleComponent: React.FunctionComponent<CounterProps> = ({
   counterExample,
   counterExampleChangeName,
   counterExampleCount,
 }) => {
-  let counterOptions = [`Counting to infinity!`, `Professional Adding Machine`, `Subtraction is in development.`]
+  let counterOptions = [
+    `Counting to infinity!`,
+    `Professional Adding Machine`,
+    `Subtraction is developed!.`,
+  ];
+  const [state, setState] = React.useState<CounterExampleActionPayload>('add');
   React.useEffect(() => {
-    setTimeout(() => {
-      counterExampleChangeName(counterOptions[Math.floor(counterOptions.length*Math.random())]);
-    }, 2000);
-    let int = setInterval(() => counterExampleCount(), 2000);
+    let int = setInterval(() => counterExampleCount(state), 1000);
     return () => {
       clearInterval(int);
     };
-  }, []);
+  }, [state]);
+
   return (
     <div className="counterExample">
       <p>{counterExample.name}</p>
       <h1>{counterExample.count}</h1>
-      <button className="counterExampleButton" onClick={()=>{counterExampleChangeName(counterOptions[Math.floor(counterOptions.length*Math.random())]);}}>New name!</button>
-      <button className="counterExampleButton" onClick={()=>{counterExampleCount()}}>Add!</button>
+      <button
+        className="counterExampleButton"
+        onClick={() => {
+          counterExampleChangeName(
+            counterOptions[Math.floor(counterOptions.length * Math.random())]
+          );
+        }}
+      >
+        New name!
+      </button>
+      {state === 'add' ? (
+        <button
+          className="counterExampleButton"
+          onClick={() => {
+            setState('subtract');
+          }}
+        >
+          Subtract!
+        </button>
+      ) : (
+        <button
+          className="counterExampleButton"
+          onClick={() => {
+            setState('add');
+          }}
+        >
+          Add!
+        </button>
+      )}
     </div>
   );
 };
@@ -49,8 +83,8 @@ const mapDispatchToProps = (dispatch: Dispatch) => {
     counterExampleChangeName: (payload: string) => {
       dispatch(counterExampleChangeName(payload));
     },
-    counterExampleCount: () => {
-      dispatch(counterExampleCount());
+    counterExampleCount: (payload: CounterExampleActionPayload) => {
+      dispatch(counterExampleCount(payload));
     },
   };
 };
